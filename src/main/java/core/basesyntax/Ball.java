95db54d8833a8070a148c4573834bcc0c1646fb2
Kamil Jan Mularski <kammul@outlook.com>
2025-09-01 19:25:38 +0200

    Create Ball.java

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class Ball {
+    
+    private final String color;
+    private final int number;
+    
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number > 100? 100 : number < 0? 0 : number;
+    }
+
+    @Override
+    public String toString() {
+        return "The ball is " +  color + ". The number is " + number + " on the ball";
+    }
+}

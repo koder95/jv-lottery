67639f29aa85d15e1b35cc751d6f1935f9996954
Kamil Jan Mularski <kammul@outlook.com>
2025-09-01 19:45:08 +0200

    Separation field from method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+    
     public String getRandomColor() {
         Color[] colors = Color.values();
         int randomIndex = random.nextInt(colors.length);

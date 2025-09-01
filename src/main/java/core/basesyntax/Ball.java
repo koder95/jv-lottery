0dc6154a4d65177581c76809709b59fec0a574e9
Kamil Jan Mularski <kammul@outlook.com>
2025-09-01 22:51:55 +0200

    Revert "Remove lines"
    This reverts commit f4f782a13f8c9508b6634d4628374ba3232efe84.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,10 +4,12 @@ public class Ball {
     
     private final Color color;
     private final int number;
+    
     public Ball(Color color, int number) {
         this.color = color;
         this.number = number;
     }
+
     @Override
     public String toString() {
         return "The ball is " + color.name().toLowerCase() + ". The number is " + number + " on the ball.";
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_VALUE = 100;
     private final ColorSupplier randomColor = new ColorSupplier();
+    
     private final Random random = new Random();
     public Ball getRandomBall() {
         return new Ball(randomColor.getRandomColor(), random.nextInt(MAX_VALUE));
